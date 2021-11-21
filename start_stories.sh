1fc2dc9cab74c757f4cb6efb21f84adf231608d3
HotDro4illa <3009kartina@gmail.com>
2021-11-21 11:25:05 +0300

    upd

---
+++ start_stories.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd /sdcard/arch
+
+instaloader --login=hot_dro4illa228 --fast-update --latest-stamps profile e_ivettta bettty.i --stories --highlights --tagged --igtv --no-captions --no-metadata-json --no-video-thumbnails
+
+python loader.py e_ivettta
+
+python merge.py e_ivettta
+
+python merge.py bettty.i
+
+python thumb.py
+
+python copier.py
+
+git add -A
+
+git commit -m "update"
+
+git push
+
+
