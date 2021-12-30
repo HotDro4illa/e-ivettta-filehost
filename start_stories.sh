7270632265a027704865530bc78dc91fb45b77f7
HotDro4illa <3009kartina@gmail.com>
2021-12-30 14:45:49 +0300

    update

--- start_stories.sh
+++ start_stories.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd /sdcard/arch/insta
-instaloader --login=hot_dro4illa228 e_ivettta bettty.i poqri polyaqurilo miss_nadiass alyona.filatova02 _history_of_loveee_ --stories --highlights --tagged --igtv --no-metadata-json --no-video-thumbnails --comments
+instaloader --fast-update --login=hot_dro4illa228 e_ivettta bettty.i poqri polyaqurilo miss_nadiass alyona.filatova02 _history_of_loveee_ --stories --highlights --tagged --igtv --no-metadata-json --no-video-thumbnails --comments
 
-instaloader --login=katerin__ea polinakyri --stories --highlights --tagged --igtv --no-metadata-json --no-video-thumbnails --comments
+instaloader --fast-update --login=katerin__ea polinakyri --stories --highlights --tagged --igtv --no-metadata-json --no-video-thumbnails --comments
 cd ..
 python merge.py insta
 python copier.py
@@ -11,6 +11,7 @@ git add -A
 git commit -m "update"
 git pull
 git push
+
 
 
 
