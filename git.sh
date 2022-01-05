1b2839d73cd53a67e256868d329792d3b263d898
HotDro4illa <3009kartina@gmail.com>
2022-01-05 21:44:03 +0300

    update

--- git.sh
+++ git.sh
@@ -4,4 +4,6 @@ git add -A
 git commit -m "update"
 git pull
 git push
+
+exit
 instaloader --fast-update --login=katerin__ea polinakyri --stories --highlights --tagged --igtv --no-metadata-json --no-video-thumbnails --comments
