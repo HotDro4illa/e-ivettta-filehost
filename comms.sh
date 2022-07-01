d6f4bf12698edf080f14a13f700b70ec2e3388f1
HotDro4illa <3009kartina@gmail.com>
2022-07-02 00:00:53 +0300

    update

--- comms.sh
+++ comms.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 termux-wake-lock
 cd /sdcard/arch/insta
-instaloader --login=hot_dro4illa228 e_ivettta bettty.i poqri polyaqurilo alyona.filatova02 --stories --highlights --tagged --igtv --no-metadata-json --no-video-thumbnails --comments --no-pictures --no-videos
+instaloader --login=hot_dro4illa228 landy2006 e_ivettta bettty.i poqri polyaqurilo alyona.filatova02 --stories --highlights --tagged --igtv --no-metadata-json --no-video-thumbnails --comments --no-pictures --no-videos
 
 
 cd ..
@@ -12,6 +12,7 @@ git commit -m "update"
 git pull
 git push
 pkill termux
+
 
 
 
