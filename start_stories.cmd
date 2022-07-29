2de8fe8ba5ddd8b21d3ac18a93bfab7575d6e3c9
HotDro4illa <49322236+HotDro4illa@users.noreply.github.com>
2022-07-29 15:45:36 +0300

    update

--- start_stories.cmd
+++ start_stories.cmd
@@ -1,5 +1,4 @@
 pip install instaloader --upgrade
-start H:\Portable\goodbyedpi-0.2.2\x86_64\goodbyedpi.exe -p -r -s -m -f 2 -k 2 -n -e 2 -a -w --allow-no-sni --auto-ttl --wrong-chksum --wrong-seq --reverse-frag --max-payload --dns-addr 77.88.8.8 --dns-port 1253
 cd insta
 instaloader --fast-update --login=hot_dro4illa228 landy2006 e_ivettta bettty.i poqri polyaqurilo alyona.filatova02 --stories --highlights --tagged --igtv --no-video-thumbnails --comments --no-compress-json --geotags --max-connection-attempts 1
 
