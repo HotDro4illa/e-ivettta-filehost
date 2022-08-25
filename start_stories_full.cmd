858a48207f5616e848360ee2cfcfe35ff5b5689e
HotDro4illa <49322236+HotDro4illa@users.noreply.github.com>
2022-08-25 12:50:23 +0300

    update

--- start_stories_full.cmd
+++ start_stories_full.cmd
@@ -1,12 +1,12 @@
 pip install instaloader --upgrade
 cd insta
-instaloader --login=hot_dro4illa228 landy2006 e_ivettta bettty.i poqri polyaqurilo alyona.filatova02 --stories --highlights --tagged --igtv --no-video-thumbnails --comments --no-compress-json --geotags --max-connection-attempts 1
+instaloader --login=hot_dro4illa228 landy2006 e_ivettta bettty.i poqri polyaqurilo alyona.filatova02 --stories --highlights --tagged --igtv --no-video-thumbnails --comments --no-compress-json --geotags
 
 
 cd ..
 python copier.py
 git add -A
-git commit -m "comment update"
+git commit -m "update"
 git pull
 git push
 pause
