8589ef1833a785b6a4ee218412c446581868f45b
HotDro4illa <49322236+HotDro4illa@users.noreply.github.com>
2022-09-10 22:09:11 +0300

    update

--- start_stories.cmd
+++ start_stories.cmd
@@ -1,6 +1,6 @@
 pip install instaloader --upgrade
 cd insta
-instaloader --fast-update --login=hot_dro4illa228 landy2006 e_ivettta bettty.i poqri polyaqurilo alyona.filatova02 --stories --highlights --tagged --igtv --no-video-thumbnails --comments --no-compress-json --geotags
+instaloader --fast-update --login=hot_dro4illa228 landy2006 e_ivettta bettty.i poqri polyaqurilo alyona.filatova02 --stories --highlights --tagged --igtv --no-video-thumbnails --comments --no-compress-json --geotags --sanitize-paths
 
 
 cd ..
