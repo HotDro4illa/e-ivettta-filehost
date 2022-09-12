4df1346addc6ab282709419dd6010fee8eec8bff
* <3009kartina@gmail.com>
2022-09-12 22:21:01 +0300

    upd

---
+++ start_stories_serv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+termux-wake-lock
+cd ~/storage/external-1/Instaloader/insta
+instaloader --fast-update --login=hot_dro4illa228 landy2006 e_ivettta bettty.i poqri polyaqurilo alyona.filatova02 --stories --highlights --tagged --igtv --no-video-thumbnails --comments --no-compress-json --geotags --sanitize-paths
+
+cd ..
+python copier.py
+git add -A
+git commit -m "update"
+git push
