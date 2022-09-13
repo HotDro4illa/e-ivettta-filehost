2fe7dd2f5f2ea116d000b3b46ac4846c51b0f1b5
* <3009kartina@gmail.com>
2022-09-13 21:48:49 +0300

    update

--- start_stories_serv.sh
+++ start_stories_serv.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 termux-wake-lock
+while :
+do
 cd ~/storage/external-1/Instaloader/insta
 instaloader --fast-update --login=hot_dro4illa228 landy2006 e_ivettta bettty.i poqri polyaqurilo alyona.filatova02 --stories --highlights --tagged --igtv --no-video-thumbnails --comments --no-compress-json --geotags --sanitize-paths
 
@@ -8,3 +10,5 @@ python copier.py
 git add -A
 git commit -m "update"
 git push
+sleep 43200
+done
