f5397012ead82395cb80e8a221848a1d74a8dfc0
* <3009kartina@gmail.com>
2022-10-19 19:47:05 +0300

    update

--- start_stories_serv.sh
+++ start_stories_serv.sh
@@ -2,6 +2,7 @@
 termux-wake-lock
 while :
 do
+pip install instaloader --upgrade
 cd ~/storage/external-1/Instaloader/insta
 instaloader --fast-update --login=hot_dro4illa228 landy2006 e_ivettta bettty.i poqri polyaqurilo alyona.filatova02 --stories --highlights --tagged --igtv --no-video-thumbnails --comments --no-compress-json --geotags --sanitize-paths
 
